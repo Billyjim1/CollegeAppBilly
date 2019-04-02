b539e4ca5c0b9b8f7c463ad6a3d5820d24f1f489
wgrim <42billys@gmail.com>
2019-04-02 09:06:50 -0400

    Inital Commit

---
+++ app/src/main/java/com/insufficientlight/CollegeAppBilly/Guardian.java
@@ -0,0 +1,38 @@
+package com.insufficientlight.CollegeAppBilly;
+
+public class Guardian extends FamilyMember
+{
+    String firstName;
+    String lastName;
+
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public Guardian()
+    {
+        super();
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/insufficientlight/CollegeAppBilly/Profile.java
@@ -0,0 +1,38 @@
+package com.insufficientlight.CollegeAppBilly;
+
+import java.util.Date;
+
+public class Profile
+{
+
+    String lastName;
+    String firstName;
+    Date dateOfBirth;
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public Profile()
+    {
+        dateOfBirth = new Date();
+    }
+
+
+}
